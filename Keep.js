9c0a0045fd9e35aea520691208504dfa5163190e
Wongsaton-git <wongsaton.supawan@gmail.com>
2025-04-16 22:35:56 +0700

    create function and body

--- Keep.js
+++ Keep.js
@@ -3,9 +3,3 @@ document.getElementById("Num1").innerHTML=number1;
 let number2=Math.floor(Math.random() * 100)
 document.getElementById("result").innerHTML= number2
 
-
- if (number1== number2) {alert("คุณถูกรางวัล")
-     
- } else {alert("คุณไม่ถูกรางวัล ว้ายๆๆ")
-     
- }
---
+++ chkhouy.js
@@ -0,0 +1,7 @@
+var check=""
+if (number1== number2) {check="คุณถูกรางวัล"
+     
+} else {check="คุณไม่ถูกรางวัล ว้ายๆๆ"
+    
+}
+document.getElementById("check").innerHTML=check;
