5ac5a88a8355f578c51859d4e679263c1183fa56
Wongsaton-git <wongsaton.supawan@gmail.com>
2025-04-16 17:51:05 +0700

    โปรแกรมหวย

--- Keep.js
+++ Keep.js
@@ -1,1 +1,3 @@
-prompt("5555")
+let number2degree=prompt("กรอกเลขหวย")
+document.getElementById("Num1").innerHTML=number2degree;
+document.getElementById("result").innerHTML= Math.floor(Math.random() * 100)
